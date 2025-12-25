deaaadb7fdba7c083c3b67ca20dcce6348aa9537
Mrhawkinghulk <mrhawkinghulk@gmail.com>
2025-12-25 10:43:28 +0530

    Create matrixmultiplier.py

---
+++ matrixmultiplier.py
@@ -0,0 +1,7 @@
+x,y=map(int,input("enter # rows and columns for matrixA").split())
+x1,x2=map(int,input("enter # rows and columns for matrixB").split())
+
+if y==x1:
+   print("AB multiplicable")
+else:
+   print("AB not multiplicable")
